e8459009f690c6cdd5dfb0cf32d76601c1c00163
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-02-12 00:12:56 +0100

    add traverser

---
+++ test/compiler/traverser.test.js
@@ -0,0 +1,42 @@
+const NODE_TYPES = require('../../bin/compiler/nodes')
+const traverser = require('../../bin/compiler/traverser')
+
+test('traverser applies visitor patter', () => {
+    // assign
+    const ast = {
+        type: NODE_TYPES.PROGRAM,
+        body: [
+            {
+                type: NODE_TYPES.CALL_EXPRESSION,
+                name: 'add',
+                params: [
+                    {
+                        type: NODE_TYPES.NUMBER_LITERAL,
+                        value: "2"
+                    },
+                    {
+                        type: NODE_TYPES.NUMBER_LITERAL,
+                        value: "1"
+                    }
+                ]
+            }
+        ]
+    };
+    
+    const visitor = {
+        [NODE_TYPES.CALL_EXPRESSION]: {
+            enter(node, parent) { node.visited = true },
+        },
+        [NODE_TYPES.NUMBER_LITERAL]: {
+            enter(node, parent) { node.visited = true },
+        }
+    };
+
+    // act
+    traverser(ast, visitor);
+
+    // assert
+    expect(ast.body[0]).toHaveProperty('visited')
+    expect(ast.body[0].params[0]).toHaveProperty('visited')
+    expect(ast.body[0].params[1]).toHaveProperty('visited')
+})
